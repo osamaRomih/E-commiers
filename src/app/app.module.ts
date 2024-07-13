8095dabf7bea9b8ff0b75fb6b33cba1a66d6cc41
osamaRomih <osamaromih2020@gmail.com>
2024-07-13 22:23:56 +0300

    e.commerce

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { CartComponent } from './components/cart/cart.component';
+import { LoginComponent } from './components/login/login.component';
+import { SingUpComponent } from './components/sing-up/sing-up.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    CartComponent,
+    LoginComponent,
+    SingUpComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
